7ad9f8cc8032979f7b9786bc7b48f3ac299239c6
DoomSkull <dfordoomskull@gmail.com>
2023-09-18 11:17:53 +0100

    formatting fix
    Signed-off-by: DoomSkull <dfordoomskull@gmail.com>

--- Cogs/Aimlabs.py
+++ Cogs/Aimlabs.py
@@ -50,7 +50,7 @@ class AimLabs(commands.Cog):
         self.data = {k: v for k, v in sorted(self.data.items(), key=lambda item: item[1]["score"], reverse=True)}
         users = [self.client.get_user(int(user)).mention for user in self.data]
         scores = [self.data[user]["score"] for user in self.data]
-        content = "#__**Leaderboard**__#\n{}".format('\n'.join('{} - {}'.format(*t) for t in zip(users, scores)))
+        content = "#__**Leaderboard**__\n{}".format('\n'.join('{} - {}'.format(*t) for t in zip(users, scores)))
         print(content)
         await self.leaderboard_message.edit(content=content)
 
